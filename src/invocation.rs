e8337194bfc2c044b6aa61c152539d306c16ddc0
Manuel Woelker <github@manuel.woelker.org>
2020-06-12 12:44:17 +0200

    tests for invocation

--- src/invocation.rs
+++ src/invocation.rs
@@ -1,25 +1,14 @@
-use crate::args::Invocation;
-use crate::cache::Cache;
-use crate::config::Configuration;
-use crate::Result;
-use crate::{NAME, VERSION};
-use anyhow::Context;
 use std::ffi::OsStr;
-use std::process::{exit, Command};
+use std::process::Command;
 
-pub fn run_invocation(invocation: Invocation, configuration: Configuration) -> Result<()> {
-    verbose!("{} {}", NAME, VERSION);
-    let cache = Cache::create(configuration)?;
-    cache.init().context("Could not initialize cache")?;
-    verbose!("Cache initialized");
-    let command_line = cache
-        .get_command_line(&invocation.command_name)
-        .with_context(|| format!("Could not run command '{}'", invocation.command_name))?;
+use anyhow::Context;
+
+use crate::cache::CommandLine;
+use crate::Result;
+
+pub fn run_invocation(command_line: CommandLine) -> Result<i32> {
     let mut command = Command::new(command_line.binary);
-    for arg in command_line.arguments {
-        command.arg(arg);
-    }
-    command.args(invocation.args);
+    command.args(command_line.arguments);
     for (key, value) in command_line.env {
         command.env(OsStr::new(&key), OsStr::new(&value));
     }
@@ -28,12 +17,66 @@ pub fn run_invocation(invocation: Invocation, configuration: Configuration) -> R
         .status()
         .with_context(|| format!("Unable to run invocation {:?}", command))?;
     let exitcode = status.code().unwrap_or(0);
-    if exitcode != 0 {
-        report!(
-            "Command '{}' terminated with exit code {}",
-            invocation.command_name,
-            exitcode
+    Ok(exitcode)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::*;
+
+    fn assert_invocation(command_line: CommandLine, expected_exit_code: i32) {
+        assert_eq!(run_invocation(command_line).unwrap(), expected_exit_code);
+    }
+
+    #[test]
+    fn assert_simple_bash() {
+        assert_invocation(
+            CommandLine {
+                binary: "bash".to_string(),
+                arguments: vec!["-c".to_string(), "echo foo".to_string()],
+                env: Default::default(),
+            },
+            0,
         );
     }
-    exit(exitcode);
+
+    #[test]
+    fn assert_exit_code() {
+        assert_invocation(
+            CommandLine {
+                binary: "bash".to_string(),
+                arguments: vec!["-c".to_string(), "exit 123".to_string()],
+                env: Default::default(),
+            },
+            123,
+        );
+    }
+
+    #[test]
+    fn assert_env_unset() {
+        assert_invocation(
+            CommandLine {
+                binary: "bash".to_string(),
+                arguments: vec!["-c".to_string(), "exit ${CODE}".to_string()],
+                env: Default::default(),
+            },
+            0,
+        );
+    }
+
+    #[test]
+    fn assert_env_set() {
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("CODE".to_string(), "42".to_string());
+        assert_invocation(
+            CommandLine {
+                binary: "bash".to_string(),
+                arguments: vec!["-c".to_string(), "exit ${CODE}".to_string()],
+                env,
+            },
+            42,
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,12 @@ pub mod platform;
 pub mod template;
 
 use crate::args::{parse_args, Args};
+use crate::cache::Cache;
 use crate::config::{get_config, CONFIG_FILENAME};
 use crate::help::print_help;
 use crate::invocation::run_invocation;
 use anyhow::Context;
+use std::process::exit;
 use std::sync::atomic::{AtomicBool, Ordering};
 
 pub const NAME: &str = env!("CARGO_PKG_NAME");
@@ -46,10 +48,26 @@ fn main() -> Result<()> {
             let configuration = get_config().unwrap_or_default();
             print_help(&configuration, &mut std::io::stdout().lock())?
         }
-        Args::Invocation(invocation) => {
-            let configuration = get_config().with_context(|| format!("Unable to load configuration, please ensure that a file called {} exists, either in the current directory or an ancestor", CONFIG_FILENAME))?;
+        Args::Invocation(mut invocation) => {
             VERBOSE.store(invocation.verbose, Ordering::Relaxed);
-            run_invocation(invocation, configuration)?
+            let configuration = get_config().with_context(|| format!("Unable to load configuration, please ensure that a file called {} exists, either in the current directory or an ancestor", CONFIG_FILENAME))?;
+            verbose!("{} {}", NAME, VERSION);
+            let cache = Cache::create(configuration)?;
+            cache.init().context("Could not initialize cache")?;
+            verbose!("Cache initialized");
+            let mut command_line = cache
+                .get_command_line(&invocation.command_name)
+                .with_context(|| format!("Could not run command '{}'", invocation.command_name))?;
+            command_line.arguments.append(&mut invocation.args);
+            let exitcode = run_invocation(command_line)?;
+            if exitcode != 0 {
+                report!(
+                    "Command '{}' terminated with exit code {}",
+                    invocation.command_name,
+                    exitcode
+                );
+            }
+            exit(exitcode);
         }
     }
     Ok(())
