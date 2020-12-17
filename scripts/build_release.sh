030a2e930500162831ead8b43dfb9fe826ff9053
Manuel Woelker <github@manuel.woelker.org>
2020-12-17 18:24:15 +0100

    fix(release): fixes for automated release build script

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -6,9 +6,12 @@ set -o pipefail
 
 cargo build --release
 #CARGO_BUILD_TARGET=x86_64-pc-windows-gnu CARGO_TARGET_DIR=target/x86_64-pc-windows-gnu cargo build --release
-CARGO_TARGET_DIR=target/x86_64-pc-windows-gnu cargo build --release --target=x86_64-pc-windows-gnu
---target=x86_64-pc-windows-gnu
+cargo build --release --target=x86_64-pc-windows-gnu
 
 (cd integration-tests && cargo test)
 
- tar -czvf target/tt.tar.gz /target/x86_64-pc-windows-gnu/release/tt.exe
+
+rm -rf target/package
+mkdir -p target/package
+mv target/x86_64-pc-windows-gnu/release/tt.exe target/release/tt target/package
+tar -C target/package -czvf target/tt.tar.gz --transform s:'./*':: .
